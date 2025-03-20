7e2fd1254efd9824dadbd5b49fc266f857e81455
khoaphan1541 <talakhoa11102004@gmail.com>
2025-03-20 11:27:07 +0700

    [add] lab-02 transposition

--- lab02/lab-02/app.py
+++ lab02/lab-02/app.py
@@ -78,7 +78,7 @@ def railfence():
 def railfence_encrypt():
     data = request.json
     plain_text = data['plain_text']
-    key = data['key']
+    key = int(data['key'])
     encrypted_text = railfence_cipher.rail_fence_encrypt(plain_text, key)
     return jsonify({'encrypted_text': encrypted_text})
 
@@ -87,7 +87,7 @@ def railfence_encrypt():
 def railfence_decrypt():
     data = request.json
     cipher_text = data['cipher_text']
-    key = data['key']
+    key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 
